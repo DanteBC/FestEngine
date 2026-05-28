235d21c49407671c4498cebb79a900f301a1d62e
DanteBC <rawnchy@gmail.com>
2026-05-28 19:38:34 +0300

    fix VLC setup order in CI

--- bin/ci.psm1
+++ bin/ci.psm1
@@ -13,18 +13,8 @@ function FestEngineGetDeps {
         $env:PYTHON_PATH = 'C:\Python39-x64'
     }
     if ($env:VLC -eq 'latest') {
-        $match = choco list vlc | Select-String -Pattern '^vlc\s+([\d\.]+)'
-        if ($match) {
-            $env:VLC_VERSION = $match.Matches.Groups[1].Value
-        } else {
-            # Fallback
-            $line = choco list vlc | Where-Object { $_ -match '^vlc\s+' } | Select-Object -First 1
-            if ($line) {
-                $env:VLC_VERSION = ($line -split '\s+')[1]
-            }
-        }
-        Write-Warning "Extracted VLC_VERSION = $env:VLC_VERSION"
         choco install -y --no-progress $env:VLC_ARCH_FLAG vlc
+        $env:VLC_VERSION = ((choco list vlc | Select-String -Pattern '^vlc\s+([\d\.]+)') -split ' ')[1]
     } else {
         $env:VLC_VERSION = $env:VLC
         choco install -y --no-progress --version $env:VLC_VERSION $env:VLC_ARCH_FLAG vlc
