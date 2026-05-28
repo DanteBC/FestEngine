e7ca86be5211286f7bc6f2560f6de4b08373d2cb
DanteBC <rawnchy@gmail.com>
2026-05-28 20:22:02 +0300

    moving to dpocker compose (without hyphen) if available

--- bin/build.sh
+++ bin/build.sh
@@ -1,5 +1,12 @@
 #!/bin/sh
 cd "$(dirname "$0")"
-docker-compose up --build
-docker cp fest-engine:/app/bin/fest_engine.tar.gz ./fest_engine-linux-x64-minimal.tar.gz
-docker-compose down
+# Используем docker compose (без дефиса) если доступен
+if docker compose version > /dev/null 2>&1; then
+    docker compose up --build
+    docker cp fest-engine:/app/bin/fest_engine.tar.gz ./fest_engine-linux-x64-minimal.tar.gz
+    docker compose down
+else
+    docker-compose up --build
+    docker cp fest-engine:/app/bin/fest_engine.tar.gz ./fest_engine-linux-x64-minimal.tar.gz
+    docker-compose down
+fi
